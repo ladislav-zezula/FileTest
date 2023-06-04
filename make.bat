f5e8ff6101d9bb406d2138851ad48ae8cad604b1
Ladislav Zezula <zezula@volny.cz>
2023-06-04 20:16:14 +0200

    test commit

--- make.bat
+++ make.bat
@@ -1,19 +1,19 @@
 @echo off
-if not "x%WDKDIR%" == "x" goto SELECT_EXE
+if not "x%WDKDIR%" == "x" goto SELECT_BINARY
 echo The WDKDIR environment variable is not set
 echo Set this variable to your WDK directory (without ending backslash)
 echo Example: set WDKDIR C:\WinDDK\6001
 pause
 goto:eof
 
-:SELECT_EXE
+:SELECT_BINARY
 set PROJECT_DIR=%~dp0
 set SAVE_PATH=%PATH%
-set EXE_NAME=FileTest
+set BINARY_NAME=FileTest
 set LANGUAGE=En
 
-:BUILD_EXE_64
-echo Building %EXE_NAME%.exe (64-bit) ...
+:BUILD_BINARY_64
+echo Building %BINARY_NAME%.exe (64-bit) ...
 set DDKBUILDENV=
 call %WDKDIR%\bin\setenv.bat %WDKDIR%\ fre x64 wlh
 cd %PROJECT_DIR%
@@ -21,8 +21,8 @@ build.exe -czgw
 del buildfre_wlh_amd64.log
 echo.
 
-:BUILD_EXE_32
-echo Building %EXE_NAME%.exe (32-bit) ...
+:BUILD_BINARY_32
+echo Building %BINARY_NAME%.exe (32-bit) ...
 set DDKBUILDENV=
 call %WDKDIR%\bin\setenv.bat %WDKDIR%\ fre w2k
 cd %PROJECT_DIR%
@@ -30,7 +30,7 @@ build.exe -czgw
 del buildfre_w2k_x86.log
 echo.
 
-:COPY_FILES
+:POST_BUILD_STEPS
 PostBuild.exe .\objfre_wlh_amd64\amd64\%EXE_NAME%.exe /tools /subsystem4
 PostBuild.exe .\objfre_w2k_x86\i386\%EXE_NAME%.exe /tools /subsystem4
 
